0139443d0408567f6cb73fb7d7f652ad2d17b024
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-09 11:54:39 +0200

    ajout d'une colonne status dans ma BDD Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809094904) do
+ActiveRecord::Schema.define(version: 20160809095248) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160809094904) do
     t.boolean  "done"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
 end
