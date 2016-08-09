c787fa79a78a80a7f960fd6bef5392f79f83c814
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-09 16:04:06 +0200

    HTML and RubyonRails Skeleton done

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  root "tasks#index"
+  resources :tasks
+  # get '/tasks', to: 'tasks#index'
 
-  get 'tasks/show'
+  # get '/tasks/:id', to: 'tasks#show'
 
-  get 'tasks/new'
+  # get '/tasks/new', to: 'tasks#new'
 
-  get 'tasks/create'
+  # post '/tasks', to: 'tasks#create'
 
-  get 'tasks/edit'
+  # get '/tasks/:id/edit', to: 'tasks#edit'
 
-  get 'tasks/update'
+  # patch '/tasks/:id', to: 'tasks#update'
 
-  get 'tasks/destroy'
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/tasks", to
-
 end
